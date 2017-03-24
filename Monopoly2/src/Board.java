9f97a29b9dbc1b9b828fc36c15d49d1174572312
michaelmallon <michael.mallon@ucdconnect.ie>
2017-03-24 13:44:04 +0000

    Add files via upload

---
+++ Monopoly2/src/Board.java
@@ -0,0 +1,66 @@
+
+public class Board {
+	
+	// Board data from http://monopoly.wikia.com/wiki/Monopoly
+	// Note, a property is a site, utility or station
+	
+	public static final int NUM_SQUARES = 40;
+		
+	private Square[] squares = new Square[NUM_SQUARES];
+	
+	Board () {
+		squares[0] = new Square("Go");
+		squares[1] = new Property("UCD Bike Shop", 60, new int[] {2,10,30,90,160,250});
+		squares[2] = new Square("Comminity Chest");
+		squares[3] = new Property("AIB UCD", 60, new int[] {4,20,60,180,320,450});
+		squares[4]  = new Square("Income Tax");
+		squares[5] = new Property("Dublin Pearse", 200, new int[] {25,50,100,200,200,200});
+		squares[6] = new Property("Centra UCD", 100, new int[] {6,30,90,270,400,550});
+		squares[7] = new Square("Chance");
+		squares[8] = new Property("Glenomena Residences", 100, new int[] {6,30,90,270,400,550});
+		squares[9] = new Property("Merville Residences", 120, new int[] {8,40,100,300,450,600});
+		squares[10] = new Square("Jail");
+		squares[11] = new Property("Belgrove Residences", 140, new int[] {10,50,150,450,625,750});
+		squares[12] = new Property("Electric Company", 150, new int[] {4,10,0,0,0,0});
+		squares[13] = new Property("Ashfield Residences", 140, new int[] {10,50,150,450,625,750});
+		squares[14] = new Property("Roebuck Residences", 160, new int[] {12,60,180,500,700,900});
+		squares[15] = new Property("Drumcondra", 200, new int[] {25,50,100,200,200,200});
+		squares[16] = new Property("Engineering Building", 180, new int[] {14,70,200,550,750,950});
+		squares[17] = new Square("Community Chest");
+		squares[18] = new Property("Newman Building", 180, new int[] {14,70,200,550,750,950});
+		squares[19] = new Property("James Joyce Library", 200, new int[] {16,80,220,600,800,1000});
+		squares[20] = new Square("Free Parking");
+		squares[21] = new Property("UCD Gym", 220, new int[] {18,90,250,700,875,1050});
+		squares[22] = new Square("Chance");
+		squares[23] = new Property("UCD Swimming Pool", 220, new int[] {18,90,250,700,875,1050});
+		squares[24]  = new Property("UCD Student Bar", 240, new int[] {20,100,300,750,925,1100});
+		squares[25] = new Property("Dublin Hueston", 200, new int[] {25,50,100,200,200,200});
+		squares[26] = new Property("Health Science Building", 260, new int[] {22,110,330,800,975,1150});
+		squares[27] = new Property("Richview Building", 260, new int[] {22,110,330,800,975,1150});
+		squares[28] = new Property("Water Works", 150, new int[] {4,10,0,0,0,0});
+		squares[29] = new Property("UCD Students Union", 280, new int[] {22,120,360,850,1025,1200});
+		squares[30] = new Square("Go To Jail");
+		squares[31] = new Property("O'Brien Centre Of Science", 300, new int[] {26,130,390,900,1100,1275});
+		squares[32] = new Property("Vetinary Building", 300, new int[] {26,130,390,900,1100,1275});
+		squares[33] = new Square("Community Chest");
+		squares[34] = new Property("Computer Science Building", 320, new int[] {28,150,450,1000,1200,1400});
+		squares[35] = new Property("Dublin Connolly", 200, new int[] {25,50,100,200,200,200});
+		squares[36] = new Square("Chance");
+		squares[37] = new Property("Quinn School of Business", 350, new int[] {35,175,500,1100,1300,1500});
+		squares[38] = new Square("Super Tax");
+		squares[39 ] = new Property("Sutherland School Of Law", 400, new int[] {50,200,600,1400,1700,2000});
+		return;
+	}
+	
+	public Square getSquare (int index) {
+		return squares[index];
+	}
+	
+	public Property getProperty (int index) {
+		return (Property) squares[index];
+	}
+	
+	public boolean isProperty (int index) {
+		return squares[index] instanceof Property;
+	}
+}
---
+++ Monopoly2/src/UI.java
@@ -0,0 +1,292 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+import java.util.ArrayList;
+
+public class UI {
+
+	private static final int FRAME_WIDTH = 1200;
+	private static final int FRAME_HEIGHT = 800;
+	private static final String CURRENCY = " Euro";
+	
+	public static final int CMD_QUIT = 0;
+	public static final int CMD_DONE = 1;
+	public static final int CMD_ROLL = 2;
+	public static final int CMD_BUY = 3;
+	public static final int CMD_PAY_RENT = 4;
+	public static final int CMD_AUCTION = 5;
+	public static final int CMD_PROPERTY = 6;
+	public static final int CMD_BALANCE = 7;
+	public static final int CMD_BANKRUPT = 8;
+	public static final int CMD_HELP = 11;
+	public static final int CMD_BUILD = 9;
+	public static final int CMD_MORTGAGE = 10;
+	
+	public static final int ERR_SYNTAX = 0;
+	public static final int ERR_DOUBLE_ROLL = 1;
+	public static final int ERR_NO_ROLL = 2;
+	public static final int ERR_INSUFFICIENT_FUNDS = 3;
+	public static final int ERR_NOT_A_PROPERTY = 4;
+	public static final int ERR_RENT_ALREADY_PAID = 5;
+	public static final int ERR_NOT_OWNED = 6;
+	public static final int ERR_IS_OWNED = 7;
+	public static final int ERR_SELF_OWNED = 8;
+	public static final int ERR_RENT_OWED= 9;
+	public static final int ERR_ALREADY_MORTGAGED = 10;
+	public static final int ERR_MAX_HOUSES = 11;
+	
+	private final String[] errorMessages = {
+		"Error: Not a valid command.",
+		"Error: Too many rolls this turn.",
+		"Error: You have a roll to play.",
+		"Error: You don't have enough money.",
+		"Error: This square is not a property.",
+		"Error: You have already paid the rent.",
+		"Error: The property is not owned.",
+		"Error: The property is already owned.",
+		"Error: You own the property.",
+		"Error: You owe rent."
+	};
+	
+	private JFrame frame = new JFrame();
+	private BoardPanel boardPanel;	
+	private InfoPanel infoPanel = new InfoPanel();
+	private CommandPanel commandPanel = new CommandPanel();
+	private String string;
+	private boolean done;
+	private int commandId;
+
+	UI (ArrayList<Player> players) {
+		boardPanel = new BoardPanel(players);
+		frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+		frame.setTitle("Monopoly");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(boardPanel, BorderLayout.LINE_START);
+		frame.add(infoPanel, BorderLayout.LINE_END);
+		frame.add(commandPanel,BorderLayout.PAGE_END);
+		frame.setResizable(false);
+		frame.setVisible(true);
+		return;
+	}
+
+// INPUT METHODS
+	
+	public void inputName (int numPlayers) {
+		if (numPlayers == 0) {
+			infoPanel.displayString("Enter new player name (" + boardPanel.getTokenName(numPlayers) + "):");			
+		} else {
+			infoPanel.displayString("Enter new player name (" + boardPanel.getTokenName(numPlayers)  +  ") or done:");
+		}
+		commandPanel.inputString();
+		string = commandPanel.getString();
+		if ( (numPlayers > 0) && (string.trim().toLowerCase().equals("done")) ) {
+			done = true;
+		} else {
+			done = false;
+		}
+		infoPanel.displayString("> " + string);
+		return;
+	}
+	
+	public void inputCommand (Player player) {
+		boolean inputValid = false;
+		do {
+			infoPanel.displayString(player + " type your command:");
+			commandPanel.inputString();
+			string = commandPanel.getString();
+			infoPanel.displayString("> " + string);
+			string = commandPanel.getString();
+			string = string.toLowerCase();
+			string = string.trim();
+			string = string.replaceAll("( )+", " ");
+			switch (string) {
+				case "quit" :
+					commandId = CMD_QUIT;
+					inputValid = true;
+					break;
+				case "done" :
+					commandId = CMD_DONE;
+					inputValid = true;
+					break;
+				case "roll" :
+					commandId = CMD_ROLL;
+					inputValid = true;
+					break;
+				case "buy" :
+					commandId = CMD_BUY;				
+					inputValid = true;
+					break;
+				case "pay rent" :
+					commandId = CMD_PAY_RENT;
+					inputValid = true;
+					break;
+				case "auction" :
+					commandId = CMD_AUCTION;
+					inputValid = true;
+					break;
+				case "property" :
+					commandId = CMD_PROPERTY;
+					inputValid = true;
+					break;
+				case "balance" :
+					commandId = CMD_BALANCE;
+					inputValid = true;
+					break;
+				case "bankrupt" :
+					commandId = CMD_BANKRUPT;
+					inputValid = true;
+					break;
+				case "help" :
+					commandId = CMD_HELP;
+					inputValid = true;
+					break;
+				case "build" :
+					commandId = CMD_BUILD;
+					inputValid = true;
+				case "mortgage" :
+					commandId = CMD_MORTGAGE;
+					inputValid = true;
+				default:
+					inputValid = false;
+				}
+			if (!inputValid) {
+				displayError(ERR_SYNTAX);
+			}
+		} while (!inputValid);
+		if (commandId == CMD_DONE) {
+			done = true;
+		} else {
+			done = false;
+		}		
+		return;
+	}
+	
+	public String getString () {
+		return string; 
+	}
+	
+	public String getTokenName (int tokenId) {
+		return boardPanel.getTokenName(tokenId);
+	}
+	
+	public int getCommandId () {
+		return commandId;
+	}
+	
+	public boolean isDone () {
+		return done;
+	}
+	
+	
+// OUTPUT METHODS
+	
+	public void display () {
+		boardPanel.refresh();
+		return;
+	}
+	
+	public void displayString (String string) {
+		infoPanel.displayString(string);
+		return;
+	}
+	
+	public void displayBankTransaction (Player player) {
+		if (player.getTransaction() >= 0) {
+			infoPanel.displayString(player + " receives " + player.getTransaction() + CURRENCY + " from the bank.");
+		} else {
+			infoPanel.displayString(player + " pays " + (-player.getTransaction()) + CURRENCY + " to the bank.");			
+		}
+		return;
+	}
+	
+	public void displayTransaction (Player fromPlayer, Player toPlayer) {
+		infoPanel.displayString(fromPlayer + " pays " + toPlayer.getTransaction() + CURRENCY + " to " + toPlayer);
+		return;
+	}
+	
+	public void displayDice (Player player, Dice dice) {
+		infoPanel.displayString(player + " rolls " + dice + ".");
+		return;
+	}
+	
+	public void displayRollDraw () {
+		infoPanel.displayString("Draw");
+		return;
+	}
+	
+	public void displayRollWinner (Player player) {
+		infoPanel.displayString(player + " wins the roll.");
+		return;
+	}
+	
+	public void displayGameOver () {
+		infoPanel.displayString("GAME OVER");
+		return;
+	}
+	
+	public void displayCommandHelp () {
+		infoPanel.displayString("Available commands: roll, pay rent, buy, property, balance, done, quit. ");
+		return;
+	}
+	
+	public void displayBalance (Player player) {
+		infoPanel.displayString(player + "'s balance is " + player.getBalance() + CURRENCY);
+		return;
+	}
+	
+	public void displayError (int errorId) {
+		infoPanel.displayString(errorMessages[errorId]);
+		return;
+	}
+	
+	public void displayPassedGo (Player player) {
+		infoPanel.displayString(player + " passed Go.");
+		return;
+	}
+	
+	public void displayLatestProperty (Player player) {
+		infoPanel.displayString(player + " bought " + player.getLatestProperty());
+		return;
+	}
+	
+	public void displayProperty (Player player) {
+		ArrayList<Property> propertyList = player.getProperties();
+		if (propertyList.size() == 0) {
+			infoPanel.displayString(player + " owns no property.");
+		} else {
+			infoPanel.displayString(player + " owns the following property...");
+			for (Property p : propertyList) {
+				infoPanel.displayString(p.getName() + ", rent " + p.getRent());				
+			}
+		}
+	}
+	
+	public void displaySquare (Player player, Board board) {
+		infoPanel.displayString(player + " arrives at " + board.getSquare(player.getPosition()).getName() + ".");
+		if (board.isProperty(player.getPosition())) {
+			Property property = board.getProperty(player.getPosition());
+			if (property.isOwned()) {
+				infoPanel.displayString("The property is owned by " + property.getOwner() + ". Rent is " + property.getRent() + CURRENCY + ".");				
+			} else {
+				infoPanel.displayString("The property is not owned. Rent is " + property.getRent() + CURRENCY + ".");								
+			}
+		}
+		return;
+	}
+	
+	public void displayAssets (Player player) {
+		infoPanel.displayString(player + " has assets of " + player.getAssets() + CURRENCY);
+		return;
+	}
+	
+	public void displayWinner (Player player) {
+		infoPanel.displayString("The winner is " + player);
+		return;
+	}
+	
+	public void displayDraw (ArrayList<Player> players) {
+		infoPanel.displayString("The following players drew the game " + players);
+		return;
+	}
+}
